405f1f4d05e09587523f5a3d70f3baf8bb3099b4
TuanTQ <tquoctua97@gmail.com>
2021-12-16 14:24:37 +0700

    setup config theme MUI

--- src/features/Product/pages/ListPage.jsx
+++ src/features/Product/pages/ListPage.jsx
@@ -1,7 +1,12 @@
+import { Box, Typography } from '@mui/material';
 import React from 'react';
 
 function ListPage(props) {
-  return <div>Product Listing Page</div>;
+  return (
+    <Box>
+      Product Listing <Typography color="primary.main">Page</Typography>
+    </Box>
+  );
 }
 
 export default ListPage;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
+import { ThemeProvider } from '@emotion/react';
+import CssBaseline from '@mui/material/CssBaseline';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { theme } from 'utils';
+import App from './App';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <CssBaseline />
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
